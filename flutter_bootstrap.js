87fc820b9a545407ab327400ce08bbd22bbb0a68
mmig01 <mmig01@users.noreply.github.com>
2024-08-31 07:55:36 +0000

    deploy: 0e7bc1800ce549899f75910f55d80186865a9610

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4182420433"
+    serviceWorkerVersion: "3858421635"
   }
 });
