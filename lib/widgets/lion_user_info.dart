9d2c821430892173dcdd328cb9058aef96c0f365
mingi <mintim01@naver.com>
2024-09-04 01:16:12 +0900

    feat: 좋아요 기능 추가

--- lib/widgets/lion_user_info.dart
+++ lib/widgets/lion_user_info.dart
@@ -58,164 +58,168 @@ class _LionUserInfoState extends State<LionUserInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (mounted) {
-          Navigator.of(context).pushAndRemoveUntil(
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    UserDetailScreen(
-                  user: widget.user,
-                  mainPicture: widget.mainPicture,
+    return SingleChildScrollView(
+      child: GestureDetector(
+        onTap: () {
+          if (mounted) {
+            Navigator.of(context).pushAndRemoveUntil(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      UserDetailScreen(
+                    user: widget.user,
+                    mainPicture: widget.mainPicture,
+                  ),
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
+                  transitionDuration:
+                      const Duration(milliseconds: 700), // 애니메이션의 길이 설정
+                  reverseTransitionDuration: const Duration(milliseconds: 700),
+                  fullscreenDialog: false,
                 ),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-                transitionDuration:
-                    const Duration(milliseconds: 700), // 애니메이션의 길이 설정
-                reverseTransitionDuration: const Duration(milliseconds: 700),
-                fullscreenDialog: false,
-              ),
-              (Route<dynamic> route) => false); // 모든 이전 화면을 제
-        }
-      },
-      child: Column(
-        children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 5,
-                  offset: const Offset(8, 8),
-                  color: Colors.black.withOpacity(0.3),
-                )
-              ],
-            ),
-            child: Container(
+                (Route<dynamic> route) => false); // 모든 이전 화면을 제
+          }
+        },
+        child: Column(
+          children: [
+            Container(
               width: 250,
+              clipBehavior: Clip.hardEdge,
               decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor, // 배경색
-                borderRadius: BorderRadius.circular(8.0), // 테두리 모서리 둥글게
+                borderRadius: BorderRadius.circular(15),
                 boxShadow: [
                   BoxShadow(
                     blurRadius: 5,
-                    offset: const Offset(3, 3),
+                    offset: const Offset(8, 8),
                     color: Colors.black.withOpacity(0.3),
                   )
                 ],
               ),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Hero(
-                    tag: widget.user.name!,
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        clipBehavior: Clip.hardEdge,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        height: 200,
-                        child: widget.user.image != null
-                            ? Container(
-                                width: double.infinity,
-                                height: double.infinity,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        widget.user.image!), // 배경 이미지 경로
-                                    fit: BoxFit.cover,
+              child: Container(
+                width: 250,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor, // 배경색
+                  borderRadius: BorderRadius.circular(8.0), // 테두리 모서리 둥글게
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      offset: const Offset(3, 3),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Hero(
+                      tag: widget.user.name!,
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          clipBehavior: Clip.hardEdge,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          height: 200,
+                          child: widget.user.image != null
+                              ? Container(
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                          widget.user.image!), // 배경 이미지 경로
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                ),
-                              )
-                            : Image.asset(widget.mainPicture)),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    widget.user.name != null
-                        ? widget.user.name!
-                        : "anonymous lion",
-                    style: const TextStyle(
-                      fontFamily: 'Sunflower-Bold',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 25,
+                                )
+                              : Image.asset(widget.mainPicture)),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      widget.user.description != null
-                          ? widget.user.description!
-                          : "no description",
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      widget.user.name != null
+                          ? widget.user.name!
+                          : "anonymous lion",
                       style: const TextStyle(
-                        fontFamily: 'Sunflower-Light',
-                        fontSize: 15,
+                        fontFamily: 'Sunflower-Bold',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 25,
                       ),
-                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    widget.user.track != null ? widget.user.track! : "no track",
-                    style: TextStyle(
-                        fontFamily: 'Sunflower-Light',
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.blue.withOpacity(0.9)),
-                  ),
-                  const SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    widget.user.mbti != null ? widget.user.mbti! : "no mbti",
-                    style: TextStyle(
-                        fontFamily: 'Sunflower-Light',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 15,
-                        color: const Color.fromARGB(255, 233, 113, 153)
-                            .withOpacity(0.9)),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          onPressed: onHeartTap,
-                          icon: Icon(isLiked
-                              ? Icons.favorite
-                              : Icons.favorite_outline),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        widget.user.description != null
+                            ? widget.user.description!
+                            : "no description",
+                        style: const TextStyle(
+                          fontFamily: 'Sunflower-Light',
+                          fontSize: 15,
                         ),
-                      ],
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  )
-                ],
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      widget.user.track != null
+                          ? widget.user.track!
+                          : "no track",
+                      style: TextStyle(
+                          fontFamily: 'Sunflower-Light',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.blue.withOpacity(0.9)),
+                    ),
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      widget.user.mbti != null ? widget.user.mbti! : "no mbti",
+                      style: TextStyle(
+                          fontFamily: 'Sunflower-Light',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                          color: const Color.fromARGB(255, 233, 113, 153)
+                              .withOpacity(0.9)),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            onPressed: onHeartTap,
+                            icon: Icon(isLiked
+                                ? Icons.favorite
+                                : Icons.favorite_outline),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
